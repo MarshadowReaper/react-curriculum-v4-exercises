5e032af678c92cd961d677268162a0ab98fb8cec
Tajee <tajeedobbins16@gmail.com>
2026-04-05 12:20:17 -0400

    Exercise 2 complete

--- src/exercises/lesson-02/studentWork.jsx
+++ src/exercises/lesson-02/studentWork.jsx
@@ -3,10 +3,26 @@
 //Import components here
 
 export default function StudentWork() {
+  const favoriteSnack = [
+    { rank: 3, snack: 'Apple' },
+    { rank: 4, snack: 'Cheez-It' },
+    { rank: 2, snack: 'Moose Munch' },
+    { rank: 1, snack: 'Iced Honey Bun' },
+  ];
+
   return (
     <div>
+      <h1>Snacks</h1>
       {/* add JSX here */}
-      <p> Student output will go here</p>
+      <p> This is my list of favorite snacks </p>
+
+      <ul>
+        {favoriteSnack
+          .toSorted((a, b) => b.rank - a.rank)
+          .map((favoriteSnack) => (
+            <li key={favoriteSnack.rank}>{favoriteSnack.snack}</li>
+          ))}
+      </ul>
     </div>
   );
 }
