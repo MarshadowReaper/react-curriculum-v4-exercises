966157bd26cdeb636545a3facaf89a5d6740b7d9
Tajee <tajeedobbins16@gmail.com>
2026-04-27 23:56:47 -0400

    Lesson 4 complete

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,15 +1,20 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
 export default function FillRefFocus() {
-  function focusInput() {}
+  const text = useRef(null);
+
+  function focusInput() {
+    text.current.focus();
+  }
 
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
 
-      <input type="text" placeholder="Type here..." />
+      <input ref={text} placeholder="Type here..." />
 
       <button onClick={focusInput}>Focus Input</button>
     </div>
   );
 }
+//I used text to initialize the useRef and I use .current and .focus to focus the keyboard to a specific DOM element.
