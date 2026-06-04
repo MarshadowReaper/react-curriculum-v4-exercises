076c5bff11ce4c7bd49c1fe085f2ec46825ae3a3
Tajee <tajeedobbins16@gmail.com>
2026-06-04 13:33:29 -0400

    checkpoint stop

--- src/exercises/lesson-10/studentWork.jsx
+++ src/exercises/lesson-10/studentWork.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-
+import { products } from './data/products.js';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import Home from './pages/Home.jsx';
 
 export default function StudentWork() {
   const [user, setUser] = useState({
@@ -14,34 +16,45 @@ export default function StudentWork() {
   }
 
   return (
-    <div
-      style={{
-        fontFamily: 'system-ui, Arial',
-        maxWidth: 900,
-        margin: '0 auto',
-      }}
-    >
-      <aside
+    <BrowserRouter>
+      <div
         style={{
-          padding: 12,
-          marginTop: 8,
-          background: '#fafafa',
-          border: '1px solid #eee',
+          fontFamily: 'system-ui, Arial',
+          maxWidth: 900,
+          margin: '0 auto',
         }}
       >
-        <h3 style={{ marginTop: 0 }}>Debug Panel</h3>
-        <p>
-          Toggle login to test protected routing behavior. When logged out,
-          typing <code>/account</code> should NOT show Account.
-        </p>
-        <button onClick={toggleLogin}>Toggle Logged In</button>
-      </aside>
+        <aside
+          style={{
+            padding: 12,
+            marginTop: 8,
+            background: '#fafafa',
+            border: '1px solid #eee',
+          }}
+        >
+          <h3 style={{ marginTop: 0 }}>Debug Panel</h3>
+          <p>
+            Toggle login to test protected routing behavior. When logged out,
+            typing <code>/account</code> should NOT show Account.
+          </p>
+          <button onClick={toggleLogin}>Toggle Logged In</button>
+        </aside>
 
-      <Header user={user} />
+        <Header user={user} />
 
-      <main style={{ padding: 12 }}></main>
+        <main style={{ padding: 12 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+            {user.isLoggedIn && <Route path="/account" element={<Account />} />}
 
-      <Footer />
-    </div>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
