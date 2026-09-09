09bb21dc8cae06c0a6a9a7ff91a29185cacacc8b
Tajee Goode <tajeedobbins16@gmail.com>
2026-09-09 16:52:42 -0400

    Update

---
+++ src/exercises/lesson-02/SnackHeader.jsx
@@ -0,0 +1,10 @@
+function SnackHeader() {
+  return (
+    <div>
+      <h1>Snacks</h1>
+
+      <p> This is my list of favorite snacks from least to greatest. </p>
+    </div>
+  );
+}
+export default SnackHeader;
